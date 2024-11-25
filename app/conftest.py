03fb9574fe76c36a52c922de75da7800d7485a5d
ALEXANDER VEGA <ajaradv@gmail.com>
2024-11-24 21:11:36 -0600

    fix pytest setup

--- app/conftest.py
+++ app/conftest.py
@@ -4,4 +4,5 @@ import os
 os.environ['PYTEST_RUNNING'] = 'true'
 
 # from cooking_core.accounts.tests.fixtures import *  # noqa: F401, F403, E402
-# from app.alice.tests.fixtures import *  # noqa: F401, F403, E402
+from app.core.tests.fixtures import *  # noqa: F401, F403, E402
+from app.todo.tests.fixtures import *  # noqa: F401, F403, E402
