c0e2e9b124bc004e3a1552c9557416c918e53010
ALEXANDER VEGA <ajaradv@gmail.com>
2025-11-12 21:29:41 -0600

    init commit

---
+++ alice/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from alice.users.models import User
+from alice.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
